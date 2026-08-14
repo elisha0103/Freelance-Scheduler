37e72fc12b77574beadd2cf41ac6beebca3f04c1
Taeyoung Jin <41459466+elisha0103@users.noreply.github.com>
2026-08-14 11:34:21 +0900

    chore: Xcode 프로젝트 초기 설정
    iOS 18+ 타겟, Firebase/Kakao SDK SPM 의존성 추가

---
+++ FreelanceScheduler/FreelanceSchedulerTests/FreelanceSchedulerTests.swift
@@ -0,0 +1,17 @@
+//
+//  FreelanceSchedulerTests.swift
+//  FreelanceSchedulerTests
+//
+//  Created by 진태영 on 8/13/26.
+//
+
+import Testing
+@testable import FreelanceScheduler
+
+struct FreelanceSchedulerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
